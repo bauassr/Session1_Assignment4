f3eaca0b96f1be6b4c4afeba440ac5090fcad7d1
bauassr <bauassr@gmail.com>
2018-03-12 14:22:28 +0530

    Add files via upload

---
+++ Assignment_1.4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 12:19:34 2018
+
+@author: singh.shivam
+"""
+# Assignment 1.4 Calculate Volume of a sphere#
+def Vol(diameter):
+      Volume= 0
+      radius = diameter/2
+      Volume=4/3*(3.14*pow(radius,3))
+      
+      return Volume
+
+print("Calculate Volume of sphere")  
+print("enter the diameter:")
+v=Vol(int(input()))
+print("Volume of sphare: ",v)
+
+
